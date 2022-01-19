55340ca5cef1541679f2d1ad06bad0c64e8c0879
all-day-and-night <jwug0821@gmail.com>
2022-01-19 17:55:46 +0900

    domain Member Entity

---
+++ jpabook/src/main/java/javashope/jpabook/domain/Member.java
@@ -0,0 +1,26 @@
+package javashope.jpabook.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // member에 의해서 mapping 되는 column
+    private List<Order> orders = new ArrayList<>();
+}
