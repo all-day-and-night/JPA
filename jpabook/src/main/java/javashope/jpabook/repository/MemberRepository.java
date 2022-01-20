c41ecc1e7e9d74337a5a2daee4b27ed44e58144d
all-day-and-night <jwug0821@gmail.com>
2022-01-20 13:39:42 +0900

    Member Repository login

---
+++ jpabook/src/main/java/javashope/jpabook/repository/MemberRepository.java
@@ -0,0 +1,44 @@
+package javashope.jpabook.repository;
+
+import javashope.jpabook.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+/**
+ * Spring bean에 등록
+ */
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+
+    /**
+     * JQPL Entity 객체에 대한 query 생성
+     * @return
+     */
+    public List<Member> findAll(){
+        List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        return result;
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
