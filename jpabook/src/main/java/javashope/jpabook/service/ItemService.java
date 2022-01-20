deb6799c59a75d402d9d987089e21086ed9b9802
all-day-and-night <jwug0821@gmail.com>
2022-01-20 14:39:55 +0900

    Item Service logic

---
+++ jpabook/src/main/java/javashope/jpabook/service/ItemService.java
@@ -0,0 +1,33 @@
+package javashope.jpabook.service;
+
+
+import javashope.jpabook.domain.item.Item;
+import javashope.jpabook.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.sava(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
+}
