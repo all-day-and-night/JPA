960d954a3b21a0f0806eaf8dde211675baa77258
all-day-and-night <jwug0821@gmail.com>
2022-01-22 13:14:01 +0900

    Member form controller

---
+++ jpabook/src/main/java/javashope/jpabook/web/MemberController.java
@@ -0,0 +1,49 @@
+package javashope.jpabook.web;
+
+
+import javashope.jpabook.domain.Address;
+import javashope.jpabook.domain.Member;
+import javashope.jpabook.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    /**
+     *
+     * @param memberForm
+     * @return
+     */
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result){
+
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
