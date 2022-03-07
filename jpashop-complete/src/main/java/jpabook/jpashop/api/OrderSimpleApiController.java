49e17a0505a15542d752c39bd77c55cc07ac6c34
all-day-and-night <jwug0821@gmail.com>
2022-03-07 23:21:08 +0900

    Order api controller + 최적화 단계

---
+++ jpashop-complete/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,87 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
+    /**
+     * 관계형 db에서는 서로가 외래키로 계속해서 참조하기 때문에 문제가 생긴다
+     * 때문에 json 형태로 리턴할 경우 DB의 참조되어있는 관계에 명시적으로 JsonIgnore를 사용해야 한다.
+     * @return
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all){
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+
+        return all;
+    }
+
+    @GetMapping("api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4(){
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName(); // Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // Lazy 초기화
+        }
+    }
+}
