79b6dbddd55230a166d25ffa0efa165fccbd402d
all-day-and-night <jwug0821@gmail.com>
2022-03-07 23:20:24 +0900

    order repository logic + fetch join 최적화

--- jpashop-complete/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop-complete/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -94,6 +95,15 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        List<Order> resultList = em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+        return resultList;
     }
 
 }
