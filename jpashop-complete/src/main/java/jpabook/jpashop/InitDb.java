f894a6de0d69b6c1c7139ffbcdf0d21111855f35
all-day-and-night <jwug0821@gmail.com>
2022-03-08 22:45:07 +0900

    initiate DB

--- jpashop-complete/src/main/java/jpabook/jpashop/InitDb.java
+++ jpashop-complete/src/main/java/jpabook/jpashop/InitDb.java
@@ -25,6 +25,7 @@ public class InitDb {
     private final InitService initService;
 
     @PostConstruct
+    @Transactional
     public void init() {
         initService.dbInit1();
         initService.dbInit2();
