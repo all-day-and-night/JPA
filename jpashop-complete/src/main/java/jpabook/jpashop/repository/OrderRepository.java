100a58f40b1b21e403ea75c972de09cb56ab0622
all-day-and-night <jwug0821@gmail.com>
2022-03-08 22:45:54 +0900

    add method for getting orderItem using fetch join

--- jpashop-complete/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop-complete/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -106,5 +106,13 @@ public class OrderRepository {
         return resultList;
     }
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
 
